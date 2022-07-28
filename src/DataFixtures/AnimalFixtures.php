c2c2ed84f2ab376a39383ec5b08833f281714163
Thomas Fouquert <tfouquert@gmail.com>
2022-07-28 12:22:25 +0200

    add_field_to_entity_animal

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -15,31 +15,41 @@ class AnimalFixtures extends Fixture
         $a1 = new Animal();
         $a1->setName('dog')
            ->setDescription('nice dog')
-           ->setImage('dog.png');
+           ->setImage('dog.png')
+           ->setWeight(25)
+           ->setDangerous(false);
         $manager->persist($a1);
 
         $a2 = new Animal();
         $a2->setName('pig')
            ->setDescription('pink pig')
-           ->setImage('pig.png');
+           ->setImage('pig.png')
+           ->setWeight(80)
+           ->setDangerous(false);
         $manager->persist($a2);
 
         $a3 = new Animal();
         $a3->setName('snake')
            ->setDescription('dangerous snake')
-           ->setImage('snake.png');
+           ->setImage('snake.png')
+           ->setWeight(10)
+           ->setDangerous(true);
         $manager->persist($a3);
 
         $a4 = new Animal();
         $a4->setName('crocodile')
            ->setDescription('dangerous crocodile')
-           ->setImage('croco.png');
+           ->setImage('croco.png')
+           ->setWeight(180)
+           ->setDangerous(true);
         $manager->persist($a4);
 
         $a5 = new Animal();
         $a5->setName('shark')
            ->setDescription('dangerous shark')
-           ->setImage('shark.png');
+           ->setImage('shark.png')
+           ->setWeight(350)
+           ->setDangerous(true);
         $manager->persist($a5);
 
         $manager->flush();
